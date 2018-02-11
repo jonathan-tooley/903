1d2a2eb1ab14dac60526d1f02fd861093fed92d4
jonathan-tooley <jonathan.tooley@jtaweb.net>
2018-02-11 19:16:14 +0000

    Tidy main

--- SOURCES/Simulator/Main.fs
+++ SOURCES/Simulator/Main.fs
@@ -9,19 +9,14 @@ open Sim900.Devices
 open Sim900.Bits
 open Sim900.Machine
 open System
-open System.Windows
-open System.Windows.Forms
 open Sim900.FileHandling
 
 
 // MAIN PROGRAM
 [<STAThread>]
 do
-    CloseReader () // force devices to initialize
-
+    //CloseReader () // force devices to initialize
     setupControlPorts ()
-    SetScale (3)
-
     MessagePut "Elliott 903 Run Console"
 
     Processor()
