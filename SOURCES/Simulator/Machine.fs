b862995fb70bd393ba6e82f66a8551c3e7cfb565
jonathan-tooley <jonathan.tooley@jtaweb.net>
2018-04-08 19:10:15 +0000

    More moves to Globals

--- SOURCES/Simulator/Machine.fs
+++ SOURCES/Simulator/Machine.fs
@@ -9,7 +9,7 @@ module Sim900.Machine
     open System.Diagnostics
     open System.IO
     open System.Collections.Generic
-
+    open Sim900.Globals
     open Sim900.Bits
     open Sim900.Telecodes
     open Sim900.Devices
@@ -17,46 +17,7 @@ module Sim900.Machine
     open Sim900.FileHandling
 
 
-    let mutable readerholdUp      = true       // true when io blocked
-    let mutable ttyDemand         = false
-   
-       
-    type machineMode =
-       | Dead                    // Emulator ends when status is set to dead
-       | Off
-       | Reset
-       | Stopped
-       | Obey                    // signal the processor to run the command on the word generator once
-       | Cycle
-       | Running
 
-    type playStyle = 
-       | None
-       | Algol
-       | Sir
-       | Fortran_903
-       | Fortran_905
-
-    let mutable play = playStyle.None
-    let mutable status = machineMode.Off
-
-    let on() = match status with
-               | Dead -> false
-               | Off  -> false
-               | _    -> true
-
-    exception Machine of string
-
-    let memorySize    = 4 * 4096
-
-    type mode =
-        | Auto
-        | Operate
-        | Test
-
-    let mutable operate           = mode.Auto                        // Keyswitch position defaults to auto
-
-    let memory: int[]     = Array.zeroCreate (8*16*1024)
 
 
     module public MachineStateHelper =
@@ -123,51 +84,9 @@ module Sim900.Machine
         // CENTRAL PROCESSING UNIT
         // Instruction decoding, interrupt handling and monitoring 
         
-        // REGISTERS etc
-        let mutable accumulator                = 0       // held as int
-        let mutable qRegister                  = 0       // extension accumulator
-        let mutable bRegisterAddr              = 1       // index register
-        let mutable scrAddr                    = 0       // sequence control register in memory
-        let mutable sequenceControlRegister    = 0       // 
-        let mutable oldSequenceControlRegister = 0       // copy of SCR before incremented in instruction decode
-        let mutable iRegister                  = 0       // function code
-        let mutable pRegister                  = 0       // peripheral i/o
-        let mutable wordGenerator              = 0       // setting of keys on control panel
 
-      // ACCESS REGISTERS
-        let AGet ()     = accumulator 
-        let APut value  = accumulator <- value &&& mask18
-        let QGet ()     = qRegister 
-        let QPut value  = qRegister   <- value &&& mask18
-        let BGet ()     = memory.[int bRegisterAddr] 
-        let BPut value  = memory.[int bRegisterAddr] <- value &&& mask18
-        let SGet ()     = sequenceControlRegister
-        let OldSGet ()  = oldSequenceControlRegister
-        let SPut value  = sequenceControlRegister 
-        let IGet ()     = iRegister
-        let WGet ()     = wordGenerator
-        let WPut value  = wordGenerator <- value &&& mask18
-
-        // MONITORING
-        let mutable iCount            = 0L                               // instructions executed since last reset
-          
                       
-        // INTERRUPTS 
-        let mutable interruptLevel             = 1                   // current interrupt level 1..4 
-        let mutable takeInterrupt              = false               // true if interrupt is outstanding
-        let mutable protect                    = false               // set true if interrupts have to be deferred
-        // NB use of five elements in following vectors is laziness to simplify initialization.
-        let levelActive      = [|false; true;  false; false; false|] // true if level n is runnable
-
-        let L1Get()     = levelActive.[1]
-        let L2Get()     = levelActive.[2]
-        let L3Get()     = levelActive.[3]
-        let LGet ()     = interruptLevel 
-
-        let interruptPending = [|false; false; false; false; false|] // true if interrupt pending 
-                                                                     // on level 1-3
-        let interruptTrace   = [|false; false; false; false; false|] // true if trace interrupt set 
-                                                                     // on level 1-3  
+ 
         let LevelCheck level = 
             if   level < 0 || level > 3 
             then raise (Machine "Interrupt level not in range 1..3")
@@ -598,19 +517,7 @@ module Sim900.Machine
         InterruptOn level
 
 
-    // MACHINE RESET
-    let Reset () =    
-        accumulator     <- 0       
-        qRegister       <- 0       
-        bRegisterAddr   <- 1       
-        scrAddr         <- 0      
-        iRegister       <- 0       
-        pRegister       <- 0      
-        interruptLevel  <- 1         
-        takeInterrupt   <- false        
-        protect         <- false 
-        status          <- machineMode.Reset
-        StartDevices ()
+
 
         //port.Write "\r\u001B\u003A"
 
