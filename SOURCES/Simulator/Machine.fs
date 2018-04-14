d5eed8b957f58d41c5cfc0aff3c70aa06bb69fbb
jonathan-tooley <jonathan.tooley@jtaweb.net>
2018-04-14 18:52:16 +0000

    Add a reference to console

--- SOURCES/Simulator/Machine.fs
+++ SOURCES/Simulator/Machine.fs
@@ -10,18 +10,14 @@ module Sim900.Machine
     open System.IO
     open System.Collections.Generic
     open Sim900.Globals
+    open Sim900.Console
     open Sim900.Bits
     open Sim900.Telecodes
     open Sim900.Devices
     open Sim900.Formatting
     open Sim900.FileHandling
 
-
-
-
-
     module public MachineStateHelper =
-
       
         // Initial instructions
         //
