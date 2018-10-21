80bc6bec48b6e0fa9a3902c0cbbe58e5a7204456
Jonathan Alves Tooley <jonathan.tooley@jtaweb.net>
2018-10-21 18:22:24 +0100

    Remove interrupt debug messages

--- SOURCES/Elliott903/Bits.fs
+++ SOURCES/Elliott903/Bits.fs
@@ -1,4 +1,4 @@
-﻿
+
 module Sim900.Bits
 
    open Sim900.Globals
@@ -280,7 +280,7 @@ module Sim900.Bits
        if ((IG1a &&& 0b10000000) = 0b10000000 && operation = ioOperation.NoOp) then operation <- ioOperation.PncOut
        if (SelectInput  <> Input.AutoIn   && operation = NoOp) then operation <- ioOperation.AutIn
        if (SelectOutput <> Output.AutoOut && operation = NoOp) then operation <- ioOperation.AutOut
-       printfn "%i | %i | %i | %i\n"  IG1a IG1b IG2a IG2b
+      
        interrupt <- Interrupt.NoInt
        
    let IOHandler () =
